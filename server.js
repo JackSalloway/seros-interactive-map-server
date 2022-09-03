f990ffe12f1a01ffdab628fd2ade184e8804d8a5
Jack Salloway <jacksalloway@hotmail.com>
2022-09-03 10:36:41 +0100

    Update cors value

--- server.js
+++ server.js
@@ -22,12 +22,14 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use(
-    cors({
-        origin: "https://seros-interactive-map.herokuapp.com/location_data",
-        credentials: true,
-    })
-);
+app.use(cors());
+
+// app.use(
+//     cors({
+//         origin: "https://seros-interactive-map.herokuapp.com/location_data",
+//         credentials: true,
+//     })
+// );
 
 app.use(cookieParser());
 
