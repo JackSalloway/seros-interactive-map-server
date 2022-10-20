79568c2b34e31b37a9313c975abbdbececcfa7b7
Jack Salloway <jacksalloway@hotmail.com>
2022-10-20 17:37:15 +0100

    Add comment

--- models/campaign.js
+++ models/campaign.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const CampaignSchema = new Schema({
     name: { type: String, required: true, minLength: 1 },
     desc: { type: String, required: true, minLength: 1 },
-    // image?
+    // map image?
+    // banner image?
 });
 
 module.exports = mongoose.model("Campaign", CampaignSchema);
