d2e2bb7d376d4a77952b22c97f19d24ae75c28b3
Jack Salloway <jacksalloway@hotmail.com>
2023-01-23 14:55:07 +0000

    Remove console.logs

--- controllers/changelogController.js
+++ controllers/changelogController.js
@@ -35,8 +35,6 @@ class ChangelogController {
                 changelogDocument = newChangelogDocument; // Update the changelogDocument variable
             }
 
-            console.log("before push", changelogDocument.changes.length);
-
             // Obtain changelog operation type and data affected
             const changelogUrl = reqUrl.replace("/", ""); // Remove the / at the start of the url
             const changelogDataArray = changelogUrl.split("_"); // Split the string into two separate words and return an array
@@ -58,17 +56,11 @@ class ChangelogController {
             };
 
             changelogDocument.changes.unshift(newChangelogData); // Push new data values into the document
-            console.log("after push", changelogDocument.changes.length);
             // Remove the earliest entry if the length of the document is greater than 50.
             // This number was picked because to me it is a nice number to show entries up to.
             // I would like to keep all log values, but as of now I am unsure how big the document will actually get when it is full (50 changes)
             while (changelogDocument.changes.length > 50) {
-                console.log(
-                    "before pop",
-                    "Changelog length at 50, removing index 49"
-                );
                 changelogDocument.changes.pop();
-                console.log("After pop", changelogDocument.length);
             }
             changelogDocument.save(); // Save document
 
