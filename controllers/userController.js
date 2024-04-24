032037fdf79d461e6464c6e070c4b1f0245ef27d
Jack Salloway <jacksalloway@hotmail.com>
2024-04-24 08:39:21 +0100

    Comment unused method

--- controllers/userController.js
+++ controllers/userController.js
@@ -127,43 +127,43 @@ class UserController {
         }
     }
 
-    async refreshToken(token) {
-        // If there is no token in the request
-        if (!token) {
-            throw new Error("Failed due to missing token");
-        }
-        // There is a token, verify it
-        let payload = null;
-        try {
-            payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
-        } catch (err) {
-            throw new Error("Token failed verification");
-        }
-        // Token is valid, check if user exists
-        const user = await User.findOne({ _id: payload.userId });
-        // User does not exist
-        if (!user) {
-            throw new Error("User does not exist within database");
-        }
-        // User exists, check if refresh token exists on user
-        if (user.refresh_token !== token) {
-            throw new Error("User token !== cookie token");
-        }
-        // Token exists, create new refresh and access tokens
-        const accessToken = createAccessToken(
-            user.id,
-            user.username,
-            user.privileged
-        );
-        const refreshToken = createRefreshToken(user.id);
-        // Update refresh token value on the database
-        User.updateOne(
-            { _id: payload.userId },
-            { $set: { refresh_token: refreshToken } }
-        ).exec();
-        // All checks passed, send new refresh and access tokens
-        return { refreshToken, accessToken };
-    }
+    // async refreshToken(token) {
+    //     // If there is no token in the request
+    //     if (!token) {
+    //         throw new Error("Failed due to missing token");
+    //     }
+    //     // There is a token, verify it
+    //     let payload = null;
+    //     try {
+    //         payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+    //     } catch (err) {
+    //         throw new Error("Token failed verification");
+    //     }
+    //     // Token is valid, check if user exists
+    //     const user = await User.findOne({ _id: payload.userId });
+    //     // User does not exist
+    //     if (!user) {
+    //         throw new Error("User does not exist within database");
+    //     }
+    //     // User exists, check if refresh token exists on user
+    //     if (user.refresh_token !== token) {
+    //         throw new Error("User token !== cookie token");
+    //     }
+    //     // Token exists, create new refresh and access tokens
+    //     const accessToken = createAccessToken(
+    //         user.id,
+    //         user.username,
+    //         user.privileged
+    //     );
+    //     const refreshToken = createRefreshToken(user.id);
+    //     // Update refresh token value on the database
+    //     User.updateOne(
+    //         { _id: payload.userId },
+    //         { $set: { refresh_token: refreshToken } }
+    //     ).exec();
+    //     // All checks passed, send new refresh and access tokens
+    //     return { refreshToken, accessToken };
+    // }
 
     async userStartup(req) {
         if (!req.cookies.access_token) {
