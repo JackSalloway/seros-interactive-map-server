fccabb3e375d53ac73414d769c67cd344c77faaf
Jack Salloway <jacksalloway@hotmail.com>
2024-05-01 14:07:37 +0100

    Assign associated locations to npcs

--- controllers/npcController.js
+++ controllers/npcController.js
@@ -8,7 +8,7 @@ class NPCController {
             JOIN location_npcs ON location_npcs.npc_id = npc.id
             JOIN location ON location.id = location_npcs.location_id
             WHERE campaign_id = '${campaignID}'`;
-            const npcs = await database.execute(npcQuery);
+            const [npcs, _npcField] = await database.execute(npcQuery);
 
             const npcQuestsQuery = `SELECT quest_id, quest_npcs.npc_id,
             quest.name AS 'quest_name' FROM quest_npcs
@@ -17,16 +17,20 @@ class NPCController {
             JOIN location_npcs on location_npcs.npc_id = quest_npcs.npc_id
             JOIN location on location.id = location_npcs.location_id
             WHERE campaign_id = '${campaignID}'`;
-            const npcQuests = await database.execute(npcQuestsQuery);
+            const [npcQuests, _npcQuestField] = await database.execute(
+                npcQuestsQuery
+            );
 
             const npcLocationsQuery = `SELECT location_id, npc_id,
             location.name AS 'location_name', latitude, longitude FROM location_npcs
             JOIN npc on npc.id = location_npcs.npc_id
             JOIN location on location.id = location_npcs.location_id
             WHERE campaign_id = '${campaignID}';`;
-            const npcLocations = await database.execute(npcLocationsQuery);
+            const [npcLocations, _npcLocationField] = await database.execute(
+                npcLocationsQuery
+            );
 
-            console.log(npcs);
+            // console.log(npcs);
             // console.log(npcQuests);
             // console.log(npcLocations);
 
@@ -42,9 +46,31 @@ class NPCController {
                     associated_locations: [],
                     associated_quests: [],
                 };
+
+                // Assign all associated locations to npcObject
+                npcLocations.forEach((npcLocation) => {
+                    // Early return if current locationQuest in loop is not related to the current quest
+                    if (npcLocation.npc_id !== npcObject.id) {
+                        return;
+                    }
+
+                    // Push associated location to relevant npcObject
+                    npcObject.associated_locations.push({
+                        id: npcLocation.location_id,
+                        name: npcLocation.location_name,
+                        latlng: {
+                            lat: npcLocation.latitude,
+                            lng: npcLocation.longitude,
+                        },
+                    });
+                });
+
+                return npcObject;
             });
 
-            return npcs[0];
+            // console.log(npcData);
+
+            // return npcs[0];
         } catch (err) {
             throw err;
         }
