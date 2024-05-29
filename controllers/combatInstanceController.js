b3e89129594dad5b1102f1651f433d37d01875f5
Jack Salloway <jacksalloway@hotmail.com>
2024-05-29 14:33:30 +0100

    Create assembleOne method on CombatInstanceController class to further isolate methods

--- controllers/combatInstanceController.js
+++ controllers/combatInstanceController.js
@@ -156,6 +156,86 @@ class CombatInstanceController {
         }
     }
 
+    // Assemble a single combat instance object with all it's turns and return it - used in create and update combat instance routes
+    async assembleOne(combatInstanceId) {
+        // Select relevant combat instance from database
+        const combatInstanceQuery = `SELECT combat_instance.id AS "combat_instance_id", combat_instance.name AS "combat_instance_name",
+        combat_instance.description, combat_instance.updated_at, location.id AS "location_id", location.name AS "location_name", latitude, longitude, campaign_id
+        FROM combat_instance
+        JOIN location ON location.id = combat_instance.location_id
+        WHERE combat_instance.id = ?;`;
+        const [combatInstance, _combatInstanceField] = await database.query(
+            combatInstanceQuery,
+            combatInstanceId
+        );
+
+        // Select relevant turns from database
+        const combatInstanceTurnsQuery = `SELECT combat_instance_player_turn.id AS "turn_id", turn_number, damage, healing,
+        combat_instance_player_turn.player_id AS "player_id", name, class
+        FROM combat_instance_player_turn
+        JOIN player ON player.id = combat_instance_player_turn.player_id
+        WHERE combat_instance_id = ?`;
+        const [combatInstanceTurns, _combatInstanceTurnsField] =
+            await database.query(combatInstanceTurnsQuery, combatInstanceId);
+
+        // Create and populate combatInstanceObject
+        const combatInstanceObject = {
+            id: combatInstance[0].combat_instance_id,
+            name: combatInstance[0].combat_instance_name,
+            description: combatInstance[0].description,
+            players: [],
+            updated_at: combatInstance[0].updated_at,
+            location: {
+                id: combatInstance[0].location_id,
+                name: combatInstance[0].location_name,
+                latlng: {
+                    lat: combatInstance[0].latitude,
+                    lng: combatInstance[0].longitude,
+                },
+            },
+            campaign: {
+                id: combatInstance[0].campaign_id,
+            },
+        };
+
+        // Populate the combatInstanceObject.players array
+        combatInstanceTurns.forEach((playerTurn) => {
+            // Check if there is already an index for current player
+            const playerIndex = combatInstanceObject.players.findIndex(
+                (player) => {
+                    return player.id === playerTurn.player_id;
+                }
+            );
+
+            if (playerIndex !== -1) {
+                // Player index already exists, so push the player_turn_id, turn_number, damage and healing values into it
+                combatInstanceObject.players[playerIndex].turns.push({
+                    id: playerTurn.player_turn_id,
+                    turn_number: playerTurn.turn_number,
+                    damage: playerTurn.damage,
+                    healing: playerTurn.healing,
+                });
+            } else {
+                // Player index does not exist yet, so create a new one with all relevant details
+                combatInstanceObject.players.push({
+                    id: playerTurn.player_id,
+                    name: playerTurn.name,
+                    class: playerTurn.class,
+                    turns: [
+                        {
+                            id: playerTurn.player_turn_id,
+                            turn_number: playerTurn.turn_number,
+                            damage: playerTurn.damage,
+                            healing: playerTurn.healing,
+                        },
+                    ],
+                });
+            }
+        });
+
+        return combatInstanceObject;
+    }
+
     // Create a combat instance at a relevant location
     async createCombatInstance(data) {
         try {
